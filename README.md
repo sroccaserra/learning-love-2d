8ec124047a64c6a076caf4ed41e501a2a73e5cb4
sroccaserra <sroccaserra@yahoo.com>
2022-10-30 16:09:40 +0100

    Switch to 288x224 resolution

--- README.md
+++ README.md
@@ -6,6 +6,22 @@ Learning how to render on scaled low resolutions with LÖVE.
 $ love .
 ```
 
+## Rationale
+
+The intention is to have a 224 px high resolution (28 8 px high tiles), with a
+~ 4:3 ratio (1.333...).
+
+A 296 px wide screen (37 8 px wide tiles) would be nice, with a 1.321.. ratio,
+but 37 x 28 tiles would be 1036 tiles. If we want to have a screen of at most
+1024 tiles (for silly reasons), we should limit to a 36 tiles wide screen.
+
+This would mean a 288x224 resolution, with a 1.2857.. ratio, let's settle for
+that.
+
+This would be a very small window on a 1080p screen, so having at least a x3
+pixel size would be nice. This would mean a 864x672 window. A 4 px pixel size
+would mean a 1152x896 window.
+
 ## References
 
 - <https://love2d.org/>
--- main.lua
+++ main.lua
@@ -1,15 +1,16 @@
+local maid64 = require 'maid64'
+
 local rotate
 local mire
+local gradient
 
 function love.load()
-  love.window.setMode(640, 640, {resizable=true, vsync=false, minwidth=200, minheight=200})
+  love.window.setMode(1152, 896, {resizable=true, vsync=false, minwidth=200, minheight=200})
 
-  --initilizing maid64 for use and set to 64x64 mode
-  maid64(64)
+  maid64.setup(288,224)
 
-  mire = love.graphics.newImage("mire.png")
-  mire:setFilter("nearest","nearest")
-
+  mire = maid64.newImage("mire.png")
+  gradient = maid64.newImage("gradient.png")
   rotate = 0
 end
 
@@ -18,34 +19,15 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setCanvas(maid64.canvas)
-  love.graphics.clear()
-  maid64_draw()
-  love.graphics.setCanvas()
-  love.graphics.draw(maid64.canvas, maid64.x,maid64.y,0,maid64.scaler,maid64.scaler)
-end
+  maid64.start()
 
-function maid64_draw()
-  love.graphics.print("Hello World!", x, 0)
-  love.graphics.draw(mire,32,32,rotate,1,1,32,32)
-end
+  love.graphics.draw(gradient,0,0)
+  love.graphics.print("Hello World!", 0, 0)
+  love.graphics.draw(mire,144,112,rotate,1,1,32,32)
 
-function maid64(pixels)
-  maid64 = {}
-  maid64.size = pixels or 64
-  maid64.scaler = love.graphics.getHeight() / maid64.size
-  maid64.x = love.graphics.getWidth()/2-(maid64.scaler*(maid64.size/2))
-  maid64.y = love.graphics.getHeight()/2-(maid64.scaler*(maid64.size/2))
-  maid64.canvas = love.graphics.newCanvas(maid64.size, maid64.size)
-  maid64.canvas:setFilter("nearest","nearest")
+  maid64.finish()
 end
 
 function love.resize(w, h)
-  if h < w then
-    maid64.scaler = h / maid64.size
-  else
-    maid64.scaler = w / maid64.size
-  end
-  maid64.x = w/2-(maid64.scaler*(maid64.size/2))
-  maid64.y = h/2-(maid64.scaler*(maid64.size/2))
+  maid64.resize(w,h)
 end
